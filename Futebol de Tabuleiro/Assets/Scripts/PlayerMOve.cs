c9e44003a370a47d103df475653c10949a49ee59
Gustavo Duarte <gustavo64duarte@gmail.com>
2020-06-08 19:19:22 -0300

    Futebol_De_Tabuleiro_08_06_2020

---
+++ Futebol de Tabuleiro/Assets/Scripts/PlayerMOve.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMOve : MonoBehaviour
+{
+    public static PlayerMOve p;
+    private float velocity = 30f;
+    private Transform targetpos;
+    public GameObject[] tiles;
+    public bool isplayermoving;
+    
+    void Start()
+    {
+       
+        p = this;
+       
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        WalkableTile();
+    }
+
+    private void FixedUpdate()
+    {
+        
+            if (gameObject.transform.position.x != targetpos.position.x && gameObject.transform.position.z != targetpos.position.z)
+            {
+                Move(targetpos);
+            }
+        
+      
+               
+        
+      
+    }
+
+  
+
+    
+    public void Move(Transform cell)
+    {      
+            gameObject.transform.position = Vector3.Lerp(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), new Vector3(cell.transform.position.x, gameObject.transform.position.y, cell.transform.position.z), 0.1f * Time.deltaTime * velocity);
+            targetpos = cell;
+
+
+    }
+
+    public void WalkableTile()
+    {
+       for(int i = 0; i < 63; i++)
+       {
+            if(tiles[i].gameObject.transform.position.x < gameObject.transform.position.x + 3 && tiles[i].gameObject.transform.position.z < gameObject.transform.position.z + 3)
+            {
+                tiles[i].GetComponent<Tile>().selectable = true;
+
+                
+                
+            }
+            else
+            {
+                tiles[i].GetComponent<Tile>().selectable = false;
+            }
+           
+       }
+    }
+
+}
+           
+   
+     
+    
+
---
+++ Futebol de Tabuleiro/Assets/Scripts/Tile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public static Tile t;
+    public bool wakable = true;
+    public bool current = false;
+    public bool target = false;
+    public bool selectable = false;
+    public bool startfirstmovement;
+    private bool playerismoving;
+    
+    void Start()
+    {
+        t = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (current)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if(target)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if(selectable)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        
+       if(PlayerMOve.p.isplayermoving == false)
+       {
+            target = true;
+            PlayerMOve.p.Move(gameObject.transform);
+            StartCoroutine(delaytomove());
+       }
+
+    }
+
+    IEnumerator delaytomove()
+    {
+        PlayerMOve.p.isplayermoving = true;
+        yield return new WaitForSeconds(1.5f);
+        target = false;
+        PlayerMOve.p.isplayermoving = false;
+
+    }
+
+
+
+}
